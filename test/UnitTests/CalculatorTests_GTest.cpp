da149d08999048f8827dbfc6965c60c1e542da32
myoung311 <Matt.Young@gtri.gatech.edu>
2022-03-25 14:24:28 -0500

    Changed GTest a little to match GUnit example

--- test/UnitTests/CalculatorTests_GTest.cpp
+++ test/UnitTests/CalculatorTests_GTest.cpp
@@ -1,7 +1,13 @@
 #include <Calculator.h>
 #include <gtest/gtest.h>
 
-TEST(CalculatorAdd, Add) {
+struct CalculatorTests : testing::Test {
+  Calculator uut;
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST(CalculatorTests, ShouldReturnCorrectSumOf2Numbers) {
   Calculator uut;
   uut.InputFirstNumber(9);
   uut.InputSecondNumber(8);
