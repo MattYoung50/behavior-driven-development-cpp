58115b282c0127e35e9080ddd8cd0578ed85c91c
myoung311 <Matt.Young@gtri.gatech.edu>
2022-07-15 07:43:36 -0500

    Add barebones test

--- test/BDD/calculator.feature
+++ test/BDD/calculator.feature
@@ -8,10 +8,17 @@ Feature: Calculator Addition
     And I press add
     And I have entered second input <input_2> into the calculator
     When I press equals
-    Then the correct <result> should be calculated
+    Then the correct result should be <result>
 
   Examples:
     | input_1 | input_2 | result |
     | 20      | 30      | 50     |
     | 2       | 5       | 7      |
     | 0       | 40      | 40     |
+
+  Scenario: When Add is pressed with a negative number, the correct result should be calculated
+    Given I have entered first input -20 into the calculator
+    And I press add
+    And I have entered second input 20 into the calculator
+    When I press equals
+    Then the correct result should be 0
--- test/BDD/step_definitions/CalculatorSteps.cpp
+++ test/BDD/step_definitions/CalculatorSteps.cpp
@@ -2,21 +2,32 @@
 #include <GUnit/GSteps.h>
 #include <GUnit/GTest.h>
 
-GSTEPS("Calc*") {
+GSTEPS("Calc*") 
+{
   Calculator calculator;
 
-  Given("I have entered first input {n} into the calculator") = [&](int n) {
+  Given("I have entered first input {n} into the calculator") = [&](int n)
+  {
     calculator.InputFirstNumber(n);
   };
 
-  Given("I have entered second input {n} into the calculator") = [&](int n) {
+  Given("I have entered second input {n} into the calculator") = [&](int n)
+  {
     calculator.InputSecondNumber(n);
   };
 
-  Given("I press add") = [&] { calculator.Add(); };
+  Given("I press add") = [&] 
+  {
+     calculator.Add();
+  };
 
-  When("I press equals") = [&] { calculator.Equals(); };
+  When("I press equals") = [&] 
+  {
+     calculator.Equals();
+  };
 
-  Then("the correct {expectedResult} should be calculated") =
-      [&](int expected) { EXPECT_EQ(expected, calculator.Result); };
+  Then("the correct result should be {expected}") = [&](int expected)
+  {
+    EXPECT_EQ(expected, calculator.Result);
+  };
 }
